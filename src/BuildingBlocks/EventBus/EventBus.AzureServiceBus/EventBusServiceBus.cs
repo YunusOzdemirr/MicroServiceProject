420af6e6dc4a1ce364802083b038aa434af4b298
Yunus <yunusozdemir468@gmail.com>
2022-10-30 00:29:50 +0300

    Azure EventBusServiceBus

---
+++ src/BuildingBlocks/EventBus/EventBus.AzureServiceBus/EventBusServiceBus.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using EventBus.Base;
+using EventBus.Base.Events;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
+
+namespace EventBus.AzureServiceBus
+{
+    public class EventBusServiceBus : BaseEventBus
+    {
+        private ITopicClient topicClient;
+        private ManagementClient managementClient;
+        private ILogger logger;
+
+
+        public EventBusServiceBus(EventBusConfig config, IServiceProvider serviceProvider) : base(config, serviceProvider)
+        {
+            logger = serviceProvider.GetService(typeof(ILogger<EventBusServiceBus>)) as ILogger<EventBusServiceBus>;
+            managementClient = new ManagementClient(config.EventBusConnectionString);
+            topicClient = createTopicClient();
+        }
+
+        private ITopicClient createTopicClient()
+        {
+            if (topicClient == null || topicClient.IsClosedOrClosing)
+                topicClient = new TopicClient(EventBusConfig.EventBusConnectionString, EventBusConfig.DefaultTopicName, RetryPolicy.Default);
+
+            //Ensure that topic already exists
+            if (!managementClient.TopicExistsAsync(EventBusConfig.DefaultTopicName).GetAwaiter().GetResult())
+                managementClient.CreateTopicAsync(EventBusConfig.DefaultTopicName).GetAwaiter().GetResult();
+            return topicClient;
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            var eventName = @event.GetType().Name;//Example: OrderCreatedIntegrationEvent
+            eventName = ProcessEventName(eventName);
+            var eventStr = JsonConvert.SerializeObject(@event);
+            var bodyArr = Encoding.UTF8.GetBytes(eventStr);
+
+            var message = new Message()
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                Body = bodyArr,
+                Label = eventName
+            };
+            topicClient.SendAsync(message);
+        }
+
+        public override void Subscribe<T, THandler>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProcessEventName(eventName);
+            if (!SubsManager.HasSubscriptionsForEvent(eventName))
+            {
+                var subscriptionClient = CreateSubscriptionClientIfNotExists(eventName);
+
+                RegisterSubscriptionClientMessageHandler(subscriptionClient);
+            }
+            logger.LogInformation("Subscribing to event {EventName} with {EventHandler}", eventName, typeof(THandler).Name);
+            SubsManager.AddSubscription<T, THandler>();
+        }
+
+        public override void UnSubscribe<T, THandler>()
+        {
+            var eventName = typeof(T).Name;
+            try
+            {
+                //Subscription will be there but we don't subscribe
+                var subscriptionClient = CreateSubscriptionClient(eventName);
+                subscriptionClient.RemoveRuleAsync(eventName).GetAwaiter().GetResult();
+            }
+            catch (MessagingEntityNotFoundException)
+            {
+                logger.LogWarning("The messaging entity {eventName} Could not be found", eventName);
+            }
+            logger.LogInformation("Unsubscribing from event {eventName}", eventName);
+            SubsManager.RemoveSubscription<T, THandler>();
+        }
+        private void RegisterSubscriptionClientMessageHandler(ISubscriptionClient subscriptionClient)
+        {
+            subscriptionClient.RegisterMessageHandler(
+                async (message, token) =>
+                {
+                    var eventName = message.Label;
+                    var messageData = Encoding.UTF8.GetString(message.Body);
+
+                    //Complete the message so that it is not received again
+                    if (await ProcessEvent(ProcessEventName(eventName), messageData))
+                        await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+                },
+            new MessageHandlerOptions(ExceptionReceivedHandler) { MaxConcurrentCalls = 10, AutoComplete = false });
+        }
+
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            var ex = exceptionReceivedEventArgs.Exception;
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            logger.LogError(ex, "ERROR handling message: {ExceptionMessage} - Context: {@ExceptionContext}", ex.Message, context);
+            return Task.CompletedTask;
+        }
+
+        private ISubscriptionClient CreateSubscriptionClientIfNotExists(string eventName)
+        {
+            var subClient = CreateSubscriptionClient(eventName);
+            var exists = managementClient.SubscriptionExistsAsync(EventBusConfig.DefaultTopicName, GetSubName(eventName)).GetAwaiter().GetResult();
+            if (!exists)
+            {
+                managementClient.CreateSubscriptionAsync(EventBusConfig.DefaultTopicName, GetSubName(eventName)).GetAwaiter().GetResult();
+                RemoveDefaultRule(subClient);
+            }
+            CreateRuleIfNotExists(ProcessEventName(eventName), subClient);
+            return subClient;
+        }
+
+        private void CreateRuleIfNotExists(string eventName, ISubscriptionClient subscriptionClient)
+        {
+            bool ruleExits;
+
+            try
+            {
+                var rule = managementClient.GetRuleAsync(EventBusConfig.DefaultTopicName, eventName, eventName).GetAwaiter().GetResult();
+                ruleExits = rule != null;
+            }
+            catch (MessagingEntityNotFoundException)
+            {
+                //Azure Management Client doesn't have RuleExists method
+                ruleExits = false;
+            }
+            if (!ruleExits)
+                subscriptionClient.AddRuleAsync(new RuleDescription
+                {
+                    Filter = new CorrelationFilter { Label = eventName },
+                    Name = eventName
+                }).GetAwaiter().GetResult();
+        }
+
+        private void RemoveDefaultRule(SubscriptionClient subscriptionClient)
+        {
+            try
+            {
+                subscriptionClient.RemoveRuleAsync(RuleDescription.DefaultRuleName).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning("The messaging entit {DefaultRuleName} Could not be found", RuleDescription.DefaultRuleName);
+            }
+        }
+
+        private SubscriptionClient CreateSubscriptionClient(string eventName)
+        {
+            return new SubscriptionClient(EventBusConfig.EventBusConnectionString, EventBusConfig.DefaultTopicName, GetSubName(eventName));
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            topicClient.CloseAsync().GetAwaiter().GetResult();
+            managementClient.CloseAsync().GetAwaiter().GetResult();
+            topicClient = null;
+            managementClient = null;
+        }
+    }
+}
+
