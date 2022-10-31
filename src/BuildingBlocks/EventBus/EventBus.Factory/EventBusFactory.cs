c1353f1adec9d47634fcf3c763c9687af3391219
Yunus <yunusozdemir468@gmail.com>
2022-10-31 14:55:58 +0300

    Create EventBusFactory.cs

---
+++ src/BuildingBlocks/EventBus/EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EventBus.AzureServiceBus;
+using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.RabbitMQ;
+
+namespace EventBus.Factory
+{
+    public static class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig config,IServiceProvider serviceProvider)
+        {
+            return config.EventBusType switch
+            {
+                EventBusType.AzureServiceBus => new EventBusServiceBus(config, serviceProvider),
+                _ => new EventBusRabbitMQ(config, serviceProvider)
+            };
+        }
+    }
+}
+
